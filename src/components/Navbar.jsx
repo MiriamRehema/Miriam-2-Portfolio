edde9a4bb0b8d119e5ae182dab57028852b0c2bc
MiriamRehema <rehemamiriam30@gmail.com>
2025-03-08 15:05:39 +0200

    added the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,30 @@
  
  function Navbar() {
    return (
-     <div>
-       <h1 className="text-3xl font-bold underline">
-       Navbar
-     </h1>
-     </div>
+    <>
+    <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 shadow-md fixed top-0 left-0 right-0 z-50 bg-white">
+        <div className="flex justify-between items-center py-4">
+            <div className="flex space-x-2">
+                <img src="https://www.codingninjas.com/assets-landing/images/CNLOGO.svg" alt="Coding Ninjas Logo" className="h-8 w-auto"/>
+                <hi className="font-semibold text-xl cursor-pointer">
+                    Miriam
+                <p className="text-sm">Web Developer</p>
+                </hi>
+            </div>
+            <div>
+                <ul className="flex space-x-4">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Experience</li>
+                    <li>Contact me</li>
+                    <li>Projects</li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div></div>
+    </div>
+    </>
    );
  }
  export default Navbar;
