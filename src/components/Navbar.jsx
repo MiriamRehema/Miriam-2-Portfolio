cbf277e5324bc5bc1b17e39c27aa3e0654eeffd7
MiriamRehema <rehemamiriam30@gmail.com>
2025-03-08 13:47:51 +0200

    all startup dependencies

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+ import React from "react";
+ 
+ function Navbar() {
+   return (
+     <div>
+       <h1 className="text-3xl font-bold underline">
+       Navbar
+     </h1>
+     </div>
+   );
+ }
+ export default Navbar;
