fb7bb7f7e538e178b29be7efc8350f4b517dda18
MiriamRehema <rehemamiriam30@gmail.com>
2025-03-15 22:04:47 +0200

    created about component

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function About() {
+    return (
+     <>
+     <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-10 bg-white"></div>
+
+     
+     
+     </>
+     
+    );
+  }
+  export default About;
