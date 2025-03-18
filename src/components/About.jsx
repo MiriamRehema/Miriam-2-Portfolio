57151a37f261d4a4b739028999d1d914e1e6982b
MiriamRehema <rehemamiriam30@gmail.com>
2025-03-18 12:51:15 +0200

    working on with the about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+
+
 function About() {
+
+ 
+
+
     return (
      <>
-     <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-10 bg-white"></div>
+     <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-20'></div>
+     <h1 className='text-3xl font-bold mb-5'>About</h1>
+     <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            
+     </p>
+     <br/>
+     <h1 classname= 'text- font-bold mb-5'>
+      Education
+     </h1>
+     
+    
+                    
+      
+
+     
 
      
      
