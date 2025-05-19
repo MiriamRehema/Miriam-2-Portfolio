9f5cc1a1da6d0ba29f7160016bfb1544e14287bf
MiriamRehema <rehemamiriam30@gmail.com>
2025-05-19 19:22:39 +0200

    created the project.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <>
+        <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-20'>
+            <h1 className='text-3xl font-bold mb-5'>Projects</h1>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            </p>
+            <br/>
+            <span>
+                <ul className='list-disc'>
+                    <li>Project 1</li>
+                    <li>Project 2</li>
+                    <li>Project 3</li>
+                    <li>Project 4</li>
+                </ul>
+            </span>
+        </div>
+        </>
+    );
+}
