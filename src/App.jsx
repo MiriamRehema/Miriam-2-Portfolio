880482bff3310274178081729620c4cf43ef3708
MiriamRehema <rehemamiriam30@gmail.com>
2025-05-28 16:06:04 +0200

    added the skills component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 function App() {
   return (
    <>
    <Navbar/>
    <Home/>
    <About/>
+   <Skills/>
    <Projects/>
    
    </>
