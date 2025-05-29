0598140aeadf2ad6e894fbbcf77d396d5d9035e3
MiriamRehema <rehemamiriam30@gmail.com>
2025-05-29 14:32:32 +0200

    Created footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BsGithub } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa"
+import { CgMail } from "react-icons/cg";
+function Footer() {
+    return(
+        <>
+        <hr/>
+        <footer className='py-12'>
+            <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 '>
+                <div className='flex flex-col items-center justify-center'>
+                    <div className='flex space-x-4'>
+                        <FaLinkedin size={24}/>
+                        <BsGithub size={24}/>
+                        <CgMail size={27}/>
+
+                    </div>
+                    <div className='mt-8 border-t border-gray-700 pt-8 flex flex-col items-center'>
+                        <p className='text-sm text-gray-500'>
+                            &copy; {new Date().getFullYear(2025)} Miriam Rehema. All rights reserved.
+                        </p>
+                        <p className='text-sm text-gray-500 mt-2'>
+                            Built by <span className='text-red-500'>&hearts;</span> MR
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+        </footer>
+        
+        </>
+    )
+}
+export default Footer;
